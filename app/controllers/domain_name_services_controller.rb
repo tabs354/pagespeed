55275eb3c256971f62b841167f1b79b46a5c1c8a
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-19 17:02:20 +0800

    Fixed the show list, it will only show the list of current logged in user

--- app/controllers/domain_name_services_controller.rb
+++ app/controllers/domain_name_services_controller.rb
@@ -4,9 +4,9 @@ class DomainNameServicesController < ApplicationController
   def index
     params[:status] = "all" if params[:status] == nil
     if params[:status] == "all"
-      @domain_name_services = DomainNameService.page(params[:page]).per(6)
+      @domain_name_services = DomainNameService.where(user_id: current_user.id).page(params[:page]).per(6)
     else
-      @domain_name_services = DomainNameService.where(status: params[:status]).page(params[:page]).per(6)
+      @domain_name_services = DomainNameService.where(status: params[:status], user_id: current_user.id).page(params[:page]).per(6)
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 require 'faker'
 
-10.times do
+5.times do
   email = Faker::JapaneseMedia::OnePiece.unique.character.delete(' ') + "@example.com"
   puts "User: #{email}"
   User.create(email: email,
               password: "password")
+end
+
+20.times do
+  dns = Faker::App.name.delete(" ")
+  dns = "www." + dns.downcase + ".com"
+  DomainNameService.create(dns: dns,
+                 status: ["off", "on"].sample,
+                 user_id: Faker::Number.within(range: 1..5))
+  puts "DNS: #{dns}"
 end
 
 user = User.first
