d4cf263246439be6e860cdd6366d55c1103e358e
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-20 11:56:52 +0800

    Fixed code in index DNS Controller

--- app/controllers/domain_name_services_controller.rb
+++ app/controllers/domain_name_services_controller.rb
@@ -3,10 +3,7 @@ class DomainNameServicesController < ApplicationController
 
   def index
     @domain_name_services = current_user.domain_name_services
-    if params[:status] == nil
-      @domain_name_services = @domain_name_services.page(params[:page]).per(6)
-    else
-      @domain_name_services = @domain_name_services.where(status: params[:status]).page(params[:page]).per(6)
-    end
+    @domain_name_services = @domain_name_services.where(status: params[:status]) if params[:status]
+    @domain_name_services = @domain_name_services.page(params[:page]).per(6)
   end
 end
