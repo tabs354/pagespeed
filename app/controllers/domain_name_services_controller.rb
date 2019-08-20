19eae07f92d4be4b0bfe7d697afaa19a682e524f
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-20 17:21:20 +0800

    Make Update Function for DNS. Clean up repeating codes

--- app/controllers/domain_name_services_controller.rb
+++ app/controllers/domain_name_services_controller.rb
@@ -1,5 +1,8 @@
 class DomainNameServicesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_dns, only: [:show, :edit, :update]
+  before_action :dns_new, only: :create
+  before_action :clear_prefix, only: [:update, :create]
 
   def index
     @domain_name_services = current_user.domain_name_services
@@ -12,34 +15,45 @@ class DomainNameServicesController < ApplicationController
   end
 
   def create
-    @domain_name_service = current_user.domain_name_services.new(dns_params)
-    @domain_name_service.dns = clear_prefix(dns_params[:dns])
     if @domain_name_service.save
       flash[:success] = "Domain Name Service was successfully created"
-      redirect_to domain_name_services_path
+      redirect_to domain_name_service_path(@domain_name_service)
     else
       render 'new'
     end
   end
 
   def show
-    @domain_name_service = current_user.domain_name_services.find(params[:id])
   end
 
   def edit
   end
 
   def update
-
+    if @domain_name_service.update(dns_params)
+      flash[:success] = "Domain Name Service was successfully updated"
+      redirect_to domain_name_service_path(@domain_name_service)
+    else
+      render "edit"
+    end
   end
   private
   def dns_params
     params.require(:domain_name_service).permit(:dns, :https, :status)
   end
 
-  def clear_prefix(dns)
+  def set_dns
+    @domain_name_service = current_user.domain_name_services.find(params[:id])
+  end
+
+  def dns_new
+    @domain_name_service = current_user.domain_name_services.new(dns_params)
+  end
+
+  def clear_prefix
+    dns = dns_params[:dns]
     dns.delete_prefix! "https://"
     dns.delete_prefix! "http://"
-    dns
+    @domain_name_service.dns = dns
   end
 end
