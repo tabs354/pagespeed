53c2411af395e56ca83ff49f3658dd2e5a16ec18
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-21 18:40:56 +0800

    Create simple Rest Client

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "homepage#index"
+
   namespace :admin do
     resources :users
   end
+
   resources :domain_name_services, except: :destroy
+
+  get '/search', to: 'searches#new'
+  post '/search', to: 'searches#show'
 end
