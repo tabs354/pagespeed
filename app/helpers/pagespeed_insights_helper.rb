4e0373785274766c6f54b379e05836e64ede1ce2
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-27 11:09:10 +0800

    Create PagespeedInsightHelper

---
+++ app/helpers/pagespeed_insights_helper.rb
@@ -0,0 +1,18 @@
+module PagespeedInsightsHelper
+  def progress(index)
+    progress = [:success, :info, :danger]
+    progress[index]
+  end
+
+  def get_data(data, content)
+    data["metrics"][content]["category"]
+  end
+
+  def get_distribution(data, content)
+    data["metrics"][content]["distributions"]
+  end
+
+  def set_proportion(distribution)
+    distribution["proportion"] * 100
+  end
+end
