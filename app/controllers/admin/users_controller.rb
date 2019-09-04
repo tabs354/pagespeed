6483946f2cf0684de3dae4b78a3f1bcf7b42160e
Ian Wu <ian.w@topskyweb.com>
2019-09-04 12:43:13 +0800

    fix controller name

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,17 @@
-module Admin
-  class UsersController < ApplicationController
-    before_action :authenticate_user!
-    before_action :redirect_if_not_admin
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :redirect_if_not_admin
 
-    def index
-      @users = User.page(params[:page]).per(6)
-    end
-    
-    private
-    def redirect_if_not_admin
-      unless current_user.admin?
-        flash[:alert] = 'Only admin can do that action!!'
-        redirect_to root_path
-      end
+  def index
+    @users = User.page(params[:page]).per(6)
+  end
+
+  private
+
+  def redirect_if_not_admin
+    unless current_user.admin?
+      flash[:alert] = 'Only admin can do that action!!'
+      redirect_to root_path
     end
   end
 end
