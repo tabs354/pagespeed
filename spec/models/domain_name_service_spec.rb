7a05a834f1f349a34f23dbfa4fe72528cf64b172
Ian Wu <ian.w@topskyweb.com>
2019-09-04 21:34:58 +0800

    adjust code formate

--- spec/models/domain_name_service_spec.rb
+++ spec/models/domain_name_service_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe DomainNameService, type: :model do
 
   it 'url will be remove prefix https:// or http://' do
-    domain_name_service = build(:domain_name_service, url: Faker::Internet.url(path: nil,scheme: 'http'))
+    domain_name_service = build(:domain_name_service, url: Faker::Internet.url(path: nil, scheme: 'http'))
     domain_name_service.clear_prefix
     expect(domain_name_service.url).to_not include('https://')
     expect(domain_name_service.url).to_not include('http://')
 
-    domain_name_service.url = Faker::Internet.url(path: nil,scheme: 'https')
+    domain_name_service.url = Faker::Internet.url(path: nil, scheme: 'https')
     domain_name_service.clear_prefix
     expect(domain_name_service.url).to_not include('https://')
     expect(domain_name_service.url).to_not include('http://')
