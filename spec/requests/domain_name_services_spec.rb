3005fc311fa56bf70851d713f01f5b852d17e8ae
bernard jeff igcasenza <bernardjeff2525@gmail.com>
2019-09-05 17:18:46 +0800

    fix#38 domain_name_services_controller use rspec edit show update

--- spec/requests/domain_name_services_spec.rb
+++ spec/requests/domain_name_services_spec.rb
@@ -12,14 +12,17 @@ RSpec.describe "DomainNameServices", type: :request do
       get domain_name_services_path
       expect(response).to have_http_status(200)
     end
-    it 'Count the list of domain_name_service in the index' do
+    it 'check the list of domain_name_service in the index' do
       sign_in admin
-      DomainNameService.create(url: 'google.com', https: true, status: :on, user: admin)
-      DomainNameService.create(url: 'onepiece.com', https: true, status: :on, user: admin)
+      domain_name_service_1 = create(:domain_name_service, user: admin)
+      domain_name_service_2 = create(:domain_name_service, user: admin)
       get domain_name_services_path
-      expect(DomainNameService.all.size).to be > 0
+      expect(response.body).to include(domain_name_service_1.url)
+      expect(response.body).to include(domain_name_service_2.url)
     end
   end
+
+
   describe 'Get New route for the domain name service' do
     let(:admin) {create(:user, role: :admin)}
     it 'will redirect to other page if not login' do
@@ -31,11 +34,6 @@ RSpec.describe "DomainNameServices", type: :request do
       get new_domain_name_service_path
       expect(response).to have_http_status(200)
     end
-    it 'check if there is a new instance Domain Name Service' do
-      sign_in admin
-      get new_domain_name_service_path
-      expect(DomainNameService.new).to be_a_new(DomainNameService)
-    end
   end
 
   describe 'Post method for create for the domain name service' do
@@ -43,8 +41,7 @@ RSpec.describe "DomainNameServices", type: :request do
     let(:staff) {create(:user, email: 'staff@foobar.com')}
     it 'will redirect to other page if not login' do
       domain_name_service = {url: 'onepiece.com', https: true, status: :on}
-      route_send = post domain_name_services_path, params: {domain_name_service: domain_name_service}
-      expect(route_send).to redirect_to(new_user_session_path)
+      post domain_name_services_path, params: {domain_name_service: domain_name_service}
       expect(response).to have_http_status(302)
     end
     it 'Check if the Domain name service was insert' do
@@ -61,11 +58,10 @@ RSpec.describe "DomainNameServices", type: :request do
     let(:admin) {create(:user, role: :admin)}
     let(:staff1) {create(:user, email: 'staff1@foobar.com')}
     let(:staff2) {create(:user, email: 'staff2@foobar.com')}
-    let(:domain_name_service_new) {create(:domain_name_service,url: 'onepiece.com', https: true, status: :on,user: staff1)}
+    let(:domain_name_service_new) {create(:domain_name_service, url: 'onepiece.com', user: staff1)}
     it 'will redirect to other page if not login' do
-      route_send = get edit_domain_name_service_path(domain_name_service_new)
+      get edit_domain_name_service_path(domain_name_service_new)
       expect(response).to have_http_status(302)
-      expect(route_send).to redirect_to(new_user_session_path)
     end
     it 'Checking who can access DomainNameService edit page if admin ,the same staff or the other staff' do
       sign_in admin
@@ -79,8 +75,7 @@ RSpec.describe "DomainNameServices", type: :request do
       sign_out staff1
 
       sign_in staff2
-      route_send = get edit_domain_name_service_path(domain_name_service_new)
-      expect(route_send).to redirect_to(root_path)
+      get edit_domain_name_service_path(domain_name_service_new)
       expect(response).to have_http_status(302)
     end
 
@@ -91,10 +86,9 @@ RSpec.describe "DomainNameServices", type: :request do
     let(:admin) {create(:user, role: :admin)}
     let(:staff1) {create(:user, email: 'staff1@foobar.com')}
     let(:staff2) {create(:user, email: 'staff2@foobar.com')}
-    let(:domain_name_service_new) {create(:domain_name_service,url: 'onepiece.com', https: true, status: :on,user: staff1)}
+    let(:domain_name_service_new) {create(:domain_name_service, url: 'onepiece.com', user: staff1)}
     it 'will redirect to other page if not login' do
-      route_send = get domain_name_service_path(domain_name_service_new)
-      expect(route_send).to redirect_to(new_user_session_path)
+      get domain_name_service_path(domain_name_service_new)
       expect(response).to have_http_status(302)
     end
     it 'Checking who can access DomainNameService edit page if admin ,the same staff or the other staff' do
@@ -109,8 +103,7 @@ RSpec.describe "DomainNameServices", type: :request do
       sign_out staff1
 
       sign_in staff2
-      route_send = get domain_name_service_path(domain_name_service_new)
-      expect(route_send).to redirect_to(root_path)
+      get domain_name_service_path(domain_name_service_new)
       expect(response).to have_http_status(302)
     end
   end
@@ -119,31 +112,28 @@ RSpec.describe "DomainNameServices", type: :request do
     let(:admin) {create(:user, role: :admin)}
     let(:staff1) {create(:user, email: 'staff1@foobar.com')}
     let(:staff2) {create(:user, email: 'staff2@foobar.com')}
-    let(:domain_name_service_new) {create(:domain_name_service,url: 'onepiece.com', https: true, status:'on',user: staff1)}
+    let(:domain_name_service_new) {create(:domain_name_service, url: 'onepiece.com', user: staff1)}
 
     it 'will redirect to other page if not login' do
-      domain_name_service_update = {url: 'twopiece.com', https: true, status: 'on',user: staff1}
-      route_check =patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      domain_name_service_update = {url: 'twopiece.com', user: staff1}
+      patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update}
       expect(response).to have_http_status(302)
-      expect(route_check).to redirect_to(new_user_session_path)
     end
 
     it 'checking who is updating the DomainNameService' do
       sign_in admin
-      domain_name_service_update = {url: 'twopiece.com', https: true, status: 'on',user: staff1}
-      route_send = patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      domain_name_service_update = {url: 'twopiece.com', https: true, status: 'on', user: staff1}
+      patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update}
       domain_name_service_new.reload
-      expect(route_send).to redirect_to(domain_name_service_path(domain_name_service_new))
       expect(domain_name_service_new.url).to eq(domain_name_service_update[:url])
       expect(domain_name_service_new.https).to eq(domain_name_service_update[:https])
       expect(domain_name_service_new.status).to eq(domain_name_service_update[:status])
       sign_out admin
 
       sign_in staff1
-      domain_name_service_update = {url: 'threepiece.com', https: true, status:'on'}
-      route_send = patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      domain_name_service_update = {url: 'threepiece.com', https: true, status: 'on'}
+      patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update}
       domain_name_service_new.reload
-      expect(route_send).to redirect_to(domain_name_service_path(domain_name_service_new))
       expect(domain_name_service_new.url).to eq(domain_name_service_update[:url])
       expect(domain_name_service_new.https).to eq(domain_name_service_update[:https])
       expect(domain_name_service_new.status).to eq(domain_name_service_update[:status])
@@ -151,12 +141,10 @@ RSpec.describe "DomainNameServices", type: :request do
 
       sign_in staff2
       domain_name_service_update = {url: 'fourpiece.com', https: false, status: :off}
-      route_send = patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update}
       expect(domain_name_service_new.url).not_to eq(domain_name_service_update[:url])
       expect(domain_name_service_new.https).not_to eq(domain_name_service_update[:https])
       expect(domain_name_service_new.status).not_to eq(domain_name_service_update[:status])
-      expect(route_send).to redirect_to(root_path)
-
     end
   end
 end
