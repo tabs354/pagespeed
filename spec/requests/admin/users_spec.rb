47116c1d9b09db54769ed88367f6deea78e6d3ee
Jasper Tabacug <tabacugjasper@gmail.com>
2019-09-05 14:21:06 +0800

    Adjust Test for view users. Remove sign for staff

--- spec/requests/admin/users_spec.rb
+++ spec/requests/admin/users_spec.rb
@@ -22,18 +22,15 @@ RSpec.describe "Admin::Users", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it 'should view user in page if admin' do
-      new_user = create(:user, email: 'another@foobar.com')
+    it 'should view user after create user admin and staff' do
+      new_user_admin = create(:user, email: 'admin@foobar.com', role: :admin)
+      new_user_staff = create(:user, email: 'staff@foobar.com', role: :staff)
+
       sign_in admin
       get admin_users_path
-      expect(response.body).to include(new_user.email)
-    end
 
-    it 'should not view user in page if not admin' do
-      new_user = create(:user, email: 'another@foobar.com')
-      sign_in staff
-      get admin_users_path
-      expect(response.body).not_to include(new_user.email)
+      expect(response.body).to include(new_user_staff.email)
+      expect(response.body).to include(new_user_admin.email)
     end
   end
 end
