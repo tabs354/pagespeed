927d8c51d07652bcd93572851745e89d4205511a
bernard jeff igcasenza <bernardjeff2525@gmail.com>
2019-09-05 14:38:04 +0800

    issue #38 domain_name_services_controller use rsec edit show update

--- spec/requests/domain_name_services_spec.rb
+++ spec/requests/domain_name_services_spec.rb
@@ -43,7 +43,8 @@ RSpec.describe "DomainNameServices", type: :request do
     let(:staff) {create(:user, email: 'staff@foobar.com')}
     it 'will redirect to other page if not login' do
       domain_name_service = {url: 'onepiece.com', https: true, status: :on}
-      post domain_name_services_path, params: {domain_name_service: domain_name_service}
+      route_send = post domain_name_services_path, params: {domain_name_service: domain_name_service}
+      expect(route_send).to redirect_to(new_user_session_path)
       expect(response).to have_http_status(302)
     end
     it 'Check if the Domain name service was insert' do
@@ -52,6 +53,108 @@ RSpec.describe "DomainNameServices", type: :request do
       expect {
         post domain_name_services_path, params: {domain_name_service: domain_name_service}
       }.to change {DomainNameService.count}
+    end
+  end
+
+
+  describe 'Get Edit method' do
+    let(:admin) {create(:user, role: :admin)}
+    let(:staff1) {create(:user, email: 'staff1@foobar.com')}
+    let(:staff2) {create(:user, email: 'staff2@foobar.com')}
+    let(:domain_name_service_new) {create(:domain_name_service,url: 'onepiece.com', https: true, status: :on,user: staff1)}
+    it 'will redirect to other page if not login' do
+      get edit_domain_name_service_path(domain_name_service_new)
+      expect(response).to have_http_status(302)
+    end
+    it 'Checking who can access DomainNameService edit page if admin ,the same staff or the other staff' do
+      sign_in admin
+      get edit_domain_name_service_path(domain_name_service_new)
+      expect(response).to have_http_status(200)
+      sign_out admin
+
+      sign_in staff1
+      get edit_domain_name_service_path(domain_name_service_new)
+      expect(response).to have_http_status(200)
+      sign_out staff1
+
+      sign_in staff2
+      get edit_domain_name_service_path(domain_name_service_new)
+      expect(response).to have_http_status(302)
+    end
+
+  end
+
+
+  describe 'get show method' do
+    let(:admin) {create(:user, role: :admin)}
+    let(:staff1) {create(:user, email: 'staff1@foobar.com')}
+    let(:staff2) {create(:user, email: 'staff2@foobar.com')}
+    let(:domain_name_service_new) {create(:domain_name_service,url: 'onepiece.com', https: true, status: :on,user: staff1)}
+    it 'will redirect to other page if not login' do
+      route_send = get domain_name_service_path(domain_name_service_new)
+      expect(route_send).to redirect_to(root_path)
+      expect(response).to have_http_status(302)
+    end
+    it 'Checking who can access DomainNameService edit page if admin ,the same staff or the other staff' do
+      sign_in admin
+      get domain_name_service_path(domain_name_service_new)
+      expect(response).to have_http_status(200)
+      sign_out admin
+
+      sign_in staff1
+      get domain_name_service_path(domain_name_service_new)
+      expect(response).to have_http_status(200)
+      sign_out staff1
+
+      sign_in staff2
+      get domain_name_service_path(domain_name_service_new)
+      expect(route_send).to redirect_to(root_path)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe 'Patch method update function' do
+    let(:admin) {create(:user, role: :admin)}
+    let(:staff1) {create(:user, email: 'staff1@foobar.com')}
+    let(:staff2) {create(:user, email: 'staff2@foobar.com')}
+    let(:domain_name_service_new) {create(:domain_name_service,url: 'onepiece.com', https: true, status:'on',user: staff1)}
+
+    it 'will redirect to other page if not login' do
+      domain_name_service_update = {url: 'twopiece.com', https: true, status: 'on',user: staff1}
+      route_check =patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      expect(response).to have_http_status(302)
+      expect(route_check).to redirect_to(new_user_session_path)
+    end
+
+    it 'checking who is updating the DomainNameService' do
+      sign_in admin
+      domain_name_service_update = {url: 'twopiece.com', https: true, status: 'on',user: staff1}
+      route_send = patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      domain_name_service_new.reload
+      expect(route_send).to redirect_to(domain_name_service_path(domain_name_service_new))
+      expect(domain_name_service_new.url).to eq(domain_name_service_update[:url])
+      expect(domain_name_service_new.https).to eq(domain_name_service_update[:https])
+      expect(domain_name_service_new.status).to eq(domain_name_service_update[:status])
+      sign_out admin
+
+      sign_in staff1
+      domain_name_service_update = {url: 'threepiece.com', https: true, status:'on'}
+      route_send = patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      domain_name_service_new.reload
+      expect(route_send).to redirect_to(domain_name_service_path(domain_name_service_new))
+      expect(domain_name_service_new.url).to eq(domain_name_service_update[:url])
+      expect(domain_name_service_new.https).to eq(domain_name_service_update[:https])
+      expect(domain_name_service_new.status).to eq(domain_name_service_update[:status])
+      sign_out staff1
+
+      sign_in staff2
+      domain_name_service_update = {url: 'fourpiece.com', https: false, status: :off}
+      route_send = patch domain_name_service_path(domain_name_service_new), params: {domain_name_service: domain_name_service_update }
+      expect(domain_name_service_new.url).not_to eq(domain_name_service_update[:url])
+      expect(domain_name_service_new.https).not_to eq(domain_name_service_update[:https])
+      expect(domain_name_service_new.status).not_to eq(domain_name_service_update[:status])
+      expect(route_send).to redirect_to(root_path)
+
     end
   end
 
