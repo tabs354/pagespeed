b7f494640f92a034352b7c7f7dfc4334594e10b2
bernard jeff igcasenza <bernardjeff2525@gmail.com>
2019-09-05 10:39:01 +0800

    Issue #34 Rspec/DomainNameService Model

--- spec/models/domain_name_service_spec.rb
+++ spec/models/domain_name_service_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe DomainNameService, type: :model do
     expect(domain_name_service.url).to_not include('http://')
   end
 
+  it 'Add https:// or http"//" to the url depends on https?' do
+
+    domain_name_service = build(:domain_name_service, url: Faker::Internet.url(path: nil, scheme: 'https'))
+    expect(domain_name_service.set_url).to include('https://')
+    expect(domain_name_service.set_url).to_not include('http://')
+
+    domain_name_service.url = Faker::Internet.url(path: nil, scheme: 'http')
+    domain_name_service.https = false
+    expect(domain_name_service.set_url).to include('http://')
+    expect(domain_name_service.set_url).to_not include('https://')
+  end
+
 end
